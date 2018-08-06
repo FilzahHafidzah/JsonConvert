739a46b05944e98216acce1414eb6a150758e6a6
SMKTIMQ 04 <smktimq04@SMKTIMQs-MacBook-Pro.local>
2018-08-06 21:14:19 +0700

    Initial commit

---
+++ app/src/main/java/com/filzah/jsonconvert/Response/Phone.kt
@@ -0,0 +1,6 @@
+package com.filzah.jsonconvert.Response
+
+class Phone {
+    var phone1 : Int? = null
+    var phone2 : Int? = null
+}
---
+++ app/src/main/java/com/filzah/jsonconvert/Response/ResponseData.kt
@@ -0,0 +1,5 @@
+package com.filzah.jsonconvert.Response
+
+class ResponseData {
+    var numPhone : List<Phone>? = null
+}
